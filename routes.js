e7eedb60fc9986496e9dec1342f07779ac4db890
Winston <winston.j.zhao@gmail.com>
2017-02-06 18:49:10 -0800

    fix area of issue

--- routes.js
+++ routes.js
@@ -33,23 +33,25 @@ router.post('/', function(req, res, next){
         partOfSpeech: '.synonym-description .txt',
         description: '.ttl'
     })(function(error, word){
-        console.log(word.word = req.body.word);
+        word.word = req.body.word;
         console.log(word);
         x('http://www.thesaurus.com/browse/hello?s=t', ['.relevancy-list ul li a .text'])(function(e, syns){
             console.log(syns);
+            var synSchemas = [];
+            for (let i = syns.length-1; i >= 0; i--) {
+                x('http://www.dictionary.com/browse/any?s=t', {
+                    partOfSpeech: '.luna-data-header .dbox-pg span span'
+                })(function(er, synData){
+                    console.log(synData, "SYNDATA");
+                })
+            }
             // word.save(function(err, word){
             //     if (err) return next(err);
             //     res.status(201);
             //     res.json(word);
             // })
-        }
-    ));
-
+        });
+    });
 });
-
-
-var getWordInfo = function(err, word) {
-
-}
 
 module.exports = router;
